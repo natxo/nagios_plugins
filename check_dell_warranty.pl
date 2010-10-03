20086d0689cfd9c69393f19a0e71386ad2a6cfbd
José Asenjo <j.asenjo@iriszorg.nl>
2010-10-03 12:06:47 +0200

    added user agent to the program so that it shows in the server logfiles; file gets dumped to disk before being processed

--- check_dell_warranty.pl
+++ check_dell_warranty.pl
@@ -21,21 +21,37 @@
 use strict;
 use warnings;
 
-#use LWP::Debug qw(+);
-#use WWW::Mechanize;
-use HTML::TableExtract;
+use WWW::Mechanize;
 
 # we will save the 'days left' field in this array. There usually are
 # two rows with this field on the $url
 my @days_left;
 
-#my $mech = WWW::Mechanize->new( autocheck => 1 );
+# this is the final number of days left we are interested in. The
+# @days_left array should have twice this number
+my $days_left;
 
-#my $url =
-#"http://support.dell.com/support/topics/global.aspx/support/my_systems_info/details?c=us&l=en&s=gen&ServiceTag=3GHKN4J";
+my $content = "/tmp/dell";
+my $mech = WWW::Mechanize->new( autocheck => 1 );
 
-my $url = "$ENV{HOME}/scripts/dell.html";
+# set the user agent that will show in dell's logs
+$mech->agent('check_dell_warranty/0.01; nagios plugin to monitor the number of days left before the warranty expires');
 
+my $url =
+"http://support.dell.com/support/topics/global.aspx/support/my_systems_info/details?c=us&l=en&s=gen&ServiceTag=3GHKN4J";
+
+# dump the page to the file $content
+$mech->get( $url, ":content_file" => "$content" );
+print $mech->dump_headers();
+
+die "cannot get the page: ", $mech->response->status_line
+    unless $mech->success;
+
+_days_warranty_left();
+
+get_days_left(@days_left);
+
+print "tag 3GHKN4J has $days_left days of warranty left\n";
 #my $page = $mech->get( "file://$url" );
 
 =pod
@@ -49,27 +65,67 @@ array reference like :
 headers => [ qw(Description Provider Warranty Start End Days) ];
 If the headers change, just change them here.
 =cut
-my @headers = qw(Description Provider Warranty Start End Days) ;
-my $te = HTML::TableExtract->new( headers => \@headers );
 
-#parse the $url 
-$te->parse_file( $url) ;
+#===  FUNCTION  ================================================================
+#         NAME:  _days_warranty_left
+#      PURPOSE:  get the remaining days of warranty
+#   PARAMETERS:  ????
+#      RETURNS:  an array with the days left of warranty (usually 2
+#                items with the same value
+#  DESCRIPTION:  ????
+#       THROWS:  no exceptions
+#     COMMENTS:  none
+#     SEE ALSO:  pod file above
+#===============================================================================
+sub _days_warranty_left {
+    use HTML::TableExtract;
+    my @headers = qw(Description Provider Warranty Start End Days) ;
+    my $te = HTML::TableExtract->new( headers => \@headers );
 
-# get the rows
-for my $ts ($te->tables) {
+    #parse the $content 
+    $te->parse_file( $content) ;
 
-    for my $row_ref ($ts->rows) {
-        # store the days left value in global @days_left
-        push @days_left, $row_ref->[5];
+    # get the rows
+    for my $ts ($te->tables) {
+
+        for my $row_ref ($ts->rows) {
+            # store the days left value in global @days_left
+            push @days_left, $row_ref->[5];
+        }
+    }
+
+    return @days_left;
+}	# ----------  end of subroutine days_warranty_left  ----------
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_days_left
+#      PURPOSE:  compare the @days_left array, get just one of the two
+#                values if they are equal
+#   PARAMETERS:  @days_left
+#      RETURNS:  $days_left
+#  DESCRIPTION:  ????
+#       THROWS:  no exceptions
+#     COMMENTS:  none
+#     SEE ALSO:  n/a
+#===============================================================================
+
+sub get_days_left {
+    if ( scalar(@days_left) == 2 ) {
+        if ($_[1] == $_[0]) {
+           $days_left = $_[1];
+           return $days_left;
+        }
+    }
+    else {
+        return "UNKONWN";
     }
 }
-
-print join( ',', @days_left ), "\n" ;
 
 =pod
 How to debug HTML::TableExtract
 Replace the $te object and for loop with this to get *all tables* and
-their location in the $url:
+their location in the $content
 
 my $te = HTML::TableExtract->new();
 $te->parse_file($url);
@@ -80,7 +136,7 @@ for my $ts ($te->tables) {
     }
 }
 
-You get all the tables in $url and the one we want is something like this:
+You get all the tables in $content and the one we want is something like this:
 
 Table found at 6,0:
 Description,Provider,Warranty Extension Notice *,Start Date,End Date,Days Left
