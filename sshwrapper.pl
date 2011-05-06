8813150f362374c49d9aa6087747041b71e807af
José Asenjo <j.asenjo@iriszorg.nl>
2011-05-06 10:22:24 +0200

    perltidy

--- sshwrapper.pl
+++ sshwrapper.pl
@@ -3,7 +3,7 @@
 #
 #         FILE:  sshwrapper.pl
 #
-#        USAGE:  ./sshwrapper.pl  
+#        USAGE:  ./sshwrapper.pl
 #
 #  DESCRIPTION:  ssh handler for firefox
 #
@@ -22,14 +22,14 @@ use strict;
 use warnings;
 
 # save the link in $url
-my $url = $ARGV[0] ;
-my @values = ( my $protocol, my $host) ;
+my $url = $ARGV[0];
+my @values = ( my $protocol, my $host );
 
 # split the url in protocol and host part
-@values = split(':', $url);
+@values = split( ':', $url );
 
 $protocol = $values[0];
-$host = $values[1];
+$host     = $values[1];
 
 # here we use the tr operator to delete the 2 slashes of $url
 $host =~ tr!//!!d;
@@ -38,14 +38,16 @@ if ( $protocol eq "rdp" ) {
     `rdesktop -u administrator $host -d IRISZORG -g 1280x1024`;
 }
 elsif ( $protocol eq "ssh" ) {
+
     #`xterm -e "slogin root\@$host"`;
     `gnome-terminal -e \'slogin root\@$host\'`;
 }
-elsif ( $protocol eq "telnet") {
+elsif ( $protocol eq "telnet" ) {
+
     # `xterm -e "telnet $host"`;
     `gnome-terminal -e \'telnet $host\'`;
 }
 
-elsif ( $protocol eq "ping") {
+elsif ( $protocol eq "ping" ) {
     `gnome-terminal -e \'ping $host\'`;
 }
